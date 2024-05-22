fa058410de31f646b549c5990019c4d31a23e534
vagrant <vagrant@homestead>
2024-05-22 15:04:35 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);   
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//根据分类列表话题
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
